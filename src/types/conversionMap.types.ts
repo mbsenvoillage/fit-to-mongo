8384599a3ceab118a0b95b4e93bc21f99846efa0
Yvon Momboisse <momboisse.yvn@gmail.com>
2024-03-22 16:20:12 +0100

    conversionmap types

--- src/types/conversionMap.types.ts
+++ src/types/conversionMap.types.ts
@@ -1,3 +1,24 @@
-interface ConversionMap {}
+interface FieldsMapping {
+  [fitFieldName: string]: string;
+}
 
-type FitMessageTypeKeys = "";
+interface DocumentReference {
+  localField: string; // Field in this document to match on
+  foreignCollection: string; // The MongoDB collection to link to
+  foreignField: string; // The field in the foreign document to match on
+}
+
+interface EmbeddedDocumentConfig {
+  messageType: string; // Identifies the FIT message type to embed
+  embedAs: string; // Property name under which the embedded document should appear
+  fieldMappings: FieldsMapping; // Mappings from FIT fields to MongoDB document fields for the embedded document
+}
+
+export interface FitConversionMap {
+  [messageType: string]: {
+    collectionName: string;
+    fields: FieldsMapping;
+    documentReferences?: Array<DocumentReference>;
+    embeddedDocuments?: Array<EmbeddedDocumentConfig>;
+  };
+}
